0d117e8f0129e7450c7c109e10006917d3d435ee
F11GAR0 <den180777@mail.ru>
2020-04-05 20:02:54 +0300

    password textbox fix

--- MIVLGU IOP Activity emulator/MainForm.Designer.cs
+++ MIVLGU IOP Activity emulator/MainForm.Designer.cs
@@ -75,6 +75,7 @@
             this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbPassword.Location = new System.Drawing.Point(6, 64);
             this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(147, 20);
             this.tbPassword.TabIndex = 4;
             // 
